fbaefba8a32ff3734a79ed3d04a9641e932cf5d9
ldemyanov <leonidemyanov@gmail.com>
2022-12-16 21:28:12 +0800

    feat: test query

--- src/pages/aboutProduct/AboutProduct.tsx
+++ src/pages/aboutProduct/AboutProduct.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+function useQuery() {
+  const { search } = useLocation();
+  return React.useMemo(() => new URLSearchParams(search), [search]);
+}
 
 function AboutProduct() {
+  const query = useQuery();
+  const params = query.entries();
+  for (const [key, value] of params) {
+    console.log(key, value);
+  }
   return <div>Product</div>;
 }
 
