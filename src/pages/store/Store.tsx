d9d44b312e93bedde7a25dc7def82036a1b0dc1c
DeevDevs <dmitriy.vnuchkov@gmail.com>
2022-12-17 18:06:58 +0400

    feat: add images for sorting block. update body scss temporarily. update store.tsx

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import CategorySearchBlock from '../../components/categorySearchBlock/CategorySearchBlock';
+import SearchField from '../../components/seachField/SearchField';
+import SearchParams from '../../components/searchParams/SearchParams';
+import SearchViewParams from '../../components/searchViewParams/SearchViewParams';
+import SortViewBlock from '../../components/sortViewBlock/SortViewBlock';
 import './store.scss';
 
 function Store() {
   return (
     <>
-      <div className="store">
-        <h2 className="store__title">Store</h2>
-      </div>
+      <SearchParams />
+      <SearchField />
+      <SearchViewParams />
+      <SortViewBlock />
+      <CategorySearchBlock />
     </>
   );
 }
