f66e1a2593f058f88167a891ff55aa4d08d9f5c9
DeevDevs <dmitriy.vnuchkov@gmail.com>
2022-12-20 17:22:41 +0400

    feat: add responsiveness to searchParams until 1000px

--- src/components/searchParams/SearchParams.tsx
+++ src/components/searchParams/SearchParams.tsx
@@ -6,24 +6,24 @@ function SearchParams() {
     <>
       <div className="search-params">
         <p className="search-params__par">Search Parameters:</p>
-        <ul className="search-params__params">
-          <li className="search-params__param" data-param="name">
+        <ul className="search-list">
+          <li className="search-list__item" data-param="name">
             Name
           </li>
-          <li className="search-params__param" data-param="price">
-            Price
-          </li>
-          <li className="search-params__param" data-param="description">
+          <li className="search-list__item" data-param="description">
             Description
           </li>
-          <li className="search-params__param" data-param="stock">
-            In stock
-          </li>
-          <li className="search-params__param" data-param="rating">
+          <li className="search-list__item" data-param="rating">
             Rating
           </li>
-          <li className="search-params__param" data-param="category">
+          <li className="search-list__item" data-param="category">
             Category
+          </li>
+          <li className="search-list__item" data-param="in-stock">
+            In stock
+          </li>
+          <li className="search-list__item" data-param="price">
+            Price
           </li>
         </ul>
       </div>
