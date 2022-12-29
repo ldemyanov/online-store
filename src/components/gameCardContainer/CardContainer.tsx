5d3862007f582c2c45c76a195aca8268710f80e2
ldemyanov <leonidemyanov@gmail.com>
2022-12-29 22:12:48 +0800

    feat: multilayouts

--- src/components/gameCardContainer/CardContainer.tsx
+++ src/components/gameCardContainer/CardContainer.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import './CardContainer.scss';
 import GameCard from '../gameCard/GameCard';
+import ListGameCard from '../listGameCard/ListGameCard';
 import { useAppSelector } from '../../store';
+import { ELayout } from '../../store/reducer/gamesReducer';
 
 function CardContainer() {
-  const { games } = useAppSelector((state) => state.gameReducer);
+  const { games, layout } = useAppSelector((state) => state.gameReducer);
 
   return (
     <div className="card-container">
-      {games.map((game) => (
-        <GameCard key={game.id} game={game} />
-      ))}
+      {games.map((game) => {
+        if (layout === ELayout.cards)
+          return <GameCard key={game.id} game={game} />;
+        else if (layout === ELayout.list)
+          return <ListGameCard key={game.id} game={game} />;
+      })}
     </div>
   );
 }
