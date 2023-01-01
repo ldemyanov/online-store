1cb9110b42e12224819ed5f7fa073e06951edf44
DeevDevs <dmitriy.vnuchkov@gmail.com>
2023-01-01 19:22:49 +0400

    feat: add 404 page and make it responsive

---
+++ src/pages/404/404page.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './404page.scss';
+import troll from './../../static/troll.png';
+import map from './../../static/map.png';
+
+function WrongDirectoryPage() {
+  return (
+    <div className="wd-page">
+      <img className="wd-page__map" src={map} alt="map image" />
+      <div className="wd-text">
+        <p className="wd-text__fof">404</p>
+        <p className="wd-text__msg">
+          This path is too dangerous. Please, find another way to the treasures.
+        </p>
+      </div>
+      <img className="wd-page__troll" src={troll} alt="troll image" />
+    </div>
+  );
+}
+
+export default WrongDirectoryPage;
