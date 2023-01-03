8e09a5b234ef64fe9ff05ca2f1385c2c169267f7
DeevDevs <dmitriy.vnuchkov@gmail.com>
2023-01-03 21:11:14 +0400

    feat: add magnifying glass functionality

--- src/pages/aboutProduct/AboutProduct.tsx
+++ src/pages/aboutProduct/AboutProduct.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './aboutProduct.scss';
 import booksIcon from './../../static/books-pile.png';
 import zoomIcon from './../../static/zoom-icon.png';
+import crossIcon from './../../static/white-cross.png';
 import CategoriesProdPage from '../../components/categoriesProdPage/CategoriesProdPage';
 import RatingDisplay from '../../components/ratingDisplay/RatingDisplay';
 import { useSearchParams } from 'react-router-dom';
@@ -49,78 +50,103 @@ function AboutProduct() {
 
   function returnGameData() {
     return (
-      <div className="pp-game">
-        <h2 className="pp-game__name">{thisGame.name}</h2>
-        <div className="pp-game__details">
-          <div className="pp-img-box">
-            <div className="pp-zoom-btn">
-              <img
-                className="pp-zoom-btn__img"
-                src={zoomIcon}
-                alt="zoom button"
+      <div>
+        <div className="pp-magnified-img dissolved hidden">
+          <img
+            className="pp-magnified-img__img"
+            src={thisGame.images[0]}
+            alt="game image"
+          />
+          <button
+            className="pp-magnified-img__close"
+            onClick={() => {
+              toggleElementDisplay('.pp-magnified-img');
+              document.body.style.overflow = 'visible';
+            }}
+          >
+            <img src={crossIcon} alt="" />
+          </button>
+        </div>
+
+        <div className="pp-game">
+          <h2 className="pp-game__name">{thisGame.name}</h2>
+          <div className="pp-game__details">
+            <div className="pp-img-box">
+              <div
+                className="pp-zoom-btn"
+                onClick={() => {
+                  toggleElementDisplay('.pp-magnified-img');
+                  document.body.style.overflow = 'hidden';
+                }}
+              >
+                <img
+                  className="pp-zoom-btn__img"
+                  src={zoomIcon}
+                  alt="zoom button"
+                />
+              </div>
+              <div
+                className="pp-img-box__img"
+                style={{ backgroundImage: `url(${thisGame.images[0]})` }}
+                data-url={thisGame.images[0]}
+              ></div>
+            </div>
+            <div className="pp-dtls-box">
+              <p className="pp-dtls-box__price">By: {thisGame.produced}</p>
+              <p className="pp-dtls-box__price">Price: {thisGame.price}$</p>
+              <p className="pp-dtls-box__stock">In stock: {thisGame.inStock}</p>
+              <div className="pp-dtls-box__rating">
+                <span className="pp-rating-name">Rating:</span>
+                <span className="pp-rating-box">
+                  <RatingDisplay rating={thisGame.rating} />
+                </span>
+              </div>
+              <CategoriesProdPage
+                {...{
+                  categories: thisGame.categories,
+                  values: [0, 1, 2, 3, 4, 5],
+                }}
               />
             </div>
-            <div
-              className="pp-img-box__img"
-              style={{ backgroundImage: `url(${thisGame.images[0]})` }}
-              data-url={thisGame.images[0]}
-            ></div>
           </div>
-          <div className="pp-dtls-box">
-            <p className="pp-dtls-box__price">By: {thisGame.produced}</p>
-            <p className="pp-dtls-box__price">Price: {thisGame.price}$</p>
-            <p className="pp-dtls-box__stock">In stock: {thisGame.inStock}</p>
-            <div className="pp-dtls-box__rating">
-              <span className="pp-rating-name">Rating:</span>
-              <span className="pp-rating-box">
-                <RatingDisplay rating={thisGame.rating} />
-              </span>
+          <div className="pp-controls">
+            <div className="pp-img-panel" onClick={(e) => displayImg(e)}>
+              <img className="pp-img-panel__img" src={thisGame.images[0]} />
+              <img className="pp-img-panel__img" src={thisGame.images[1]} />
+              <img className="pp-img-panel__img" src={thisGame.images[2]} />
+              <img className="pp-img-panel__img" src={thisGame.images[3]} />
             </div>
-            <CategoriesProdPage
-              {...{
-                categories: thisGame.categories,
-                values: [0, 1, 2, 3, 4, 5],
-              }}
+            <button
+              className="pp-controls__btn btn-send-to-cart"
+              onClick={() => addGameToCart(thisGame)}
+            >
+              Send to cart
+            </button>
+            <Link to={`/cart`}>
+              <button
+                className="pp-controls__btn btn-buy-now"
+                onClick={() => {
+                  setTimeout(() => {
+                    toggleElementDisplay('.pchs-module');
+                    toggleElementDisplay('.pchs-module-overlay');
+                    document.body.style.overflow = 'hidden';
+                  }, 100);
+                }}
+              >
+                Buy now
+              </button>
+            </Link>
+          </div>
+          <div className="pp-game__split-line">
+            <img
+              className="pp-game__decor"
+              src={booksIcon}
+              alt="pile of books image"
             />
           </div>
+          <h3 className="pp-game__descr-head">Description:</h3>
+          <p className="pp-game__description">{thisGame.description}</p>
         </div>
-        <div className="pp-controls">
-          <div className="pp-img-panel" onClick={(e) => displayImg(e)}>
-            <img className="pp-img-panel__img" src={thisGame.images[0]} />
-            <img className="pp-img-panel__img" src={thisGame.images[1]} />
-            <img className="pp-img-panel__img" src={thisGame.images[2]} />
-            <img className="pp-img-panel__img" src={thisGame.images[3]} />
-          </div>
-          <button
-            className="pp-controls__btn btn-send-to-cart"
-            onClick={() => addGameToCart(thisGame)}
-          >
-            Send to cart
-          </button>
-          <Link to={`/cart`}>
-            <button
-              className="pp-controls__btn btn-buy-now"
-              onClick={() => {
-                setTimeout(() => {
-                  toggleElementDisplay('.pchs-module');
-                  toggleElementDisplay('.pchs-module-overlay');
-                  document.body.style.overflow = 'hidden';
-                }, 100);
-              }}
-            >
-              Buy now
-            </button>
-          </Link>
-        </div>
-        <div className="pp-game__split-line">
-          <img
-            className="pp-game__decor"
-            src={booksIcon}
-            alt="pile of books image"
-          />
-        </div>
-        <h3 className="pp-game__descr-head">Description:</h3>
-        <p className="pp-game__description">{thisGame.description}</p>
       </div>
     );
   }
@@ -136,7 +162,11 @@ function displayImg(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
   const mainImgBlock = document.querySelector(
     '.pp-img-box__img'
   ) as HTMLElement;
+  const magnifiedImg = document.querySelector(
+    '.pp-magnified-img__img'
+  ) as HTMLImageElement;
   mainImgBlock.style.backgroundImage = `url(${chosenImage.src})`;
+  magnifiedImg.src = chosenImage.src;
   mainImgBlock.dataset.url = chosenImage.src;
 }
 
