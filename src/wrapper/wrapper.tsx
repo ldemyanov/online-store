5bba9c609db2b53e7132ea81bce931bc1f35ef9d
DeevDevs <dmitriy.vnuchkov@gmail.com>
2023-01-05 22:22:59 +0400

    feat: add spider web on bg

--- src/wrapper/wrapper.tsx
+++ src/wrapper/wrapper.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './wrapper.scss';
 import Footer from '../components/footer/Footer';
 import Header from '../components/header/header';
+import spiderWeb from './../static/bg-web.png';
 
 type WrapperProps = {
   children: React.ReactNode;
@@ -10,6 +11,7 @@ type WrapperProps = {
 function Wrapper({ children }: WrapperProps) {
   return (
     <div className="wrapper">
+      <img className="wrapper-web" src={spiderWeb} />
       <Header />
       <div className="wrapper__main">{children}</div>
       <Footer />
